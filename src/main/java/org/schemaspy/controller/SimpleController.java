3ab862ef1544b627bf0f4896662298efbbe6e636
matanki.saito <matanki.saito@gmail.com>
2019-09-14 23:26:21 +0900

    修正  - pathの先頭に/があるとファイルができない

--- src/main/java/org/schemaspy/controller/SimpleController.java
+++ src/main/java/org/schemaspy/controller/SimpleController.java
@@ -32,7 +32,7 @@ public class SimpleController {
                 "-db", databaseName,
                 "-u", userName,
                 "-p", password,
-                "-o", "/output",
+                "-o", "output/schemaspy",
                 "-dp", "ext-lib/mysql-connector-java-8.0.17.jar",
                 "-connprops", String.format("serverTimezone\\=%s", timezone),
                 "-s", databaseName
