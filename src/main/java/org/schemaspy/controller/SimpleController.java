f4f2e81d672f68537727a96c3eecb0dcafad3385
matanki.saito <matanki.saito@gmail.com>
2019-09-14 17:45:05 +0900

    実装  - 外から実行できるようにコントローラを用意した。/runで解析実行する。パラメータはgetのオプションとして渡す。/runnableで実行中かどうかを確認できる。

---
+++ src/main/java/org/schemaspy/controller/SimpleController.java
@@ -0,0 +1,52 @@
+package org.schemaspy.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.schemaspy.cli.AsyncTaskRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class SimpleController {
+    private final AsyncTaskRunner schemaSpyRunner;
+
+    private CompletableFuture<String> currentFeature;
+
+    @GetMapping("/run")
+    public void run(
+            @RequestParam(value = "type", required = false, defaultValue = "mysql") String databaseType,
+            @RequestParam(value = "host") String host,
+            @RequestParam(value = "database") String databaseName,
+            @RequestParam(value = "user") String userName,
+            @RequestParam(value = "pass") String password,
+            @RequestParam(value = "timezone", required = false, defaultValue = "JST") String timezone
+    ) {
+        final String[] args = {
+                "-t", databaseType,
+                "-host", host,
+                "-db", databaseName,
+                "-u", userName,
+                "-p", password,
+                "-o", "target/somefolder",
+                "-dp", "ext-lib/mysql-connector-java-8.0.17.jar",
+                "-tz", timezone,
+                "-s", databaseName
+        };
+
+        if (currentFeature == null || currentFeature.isDone()) {
+            currentFeature = schemaSpyRunner.asyncRun(args);
+        } else {
+            log.info("working");
+        }
+    }
+
+    @GetMapping("/runnable")
+    public boolean check() {
+        return currentFeature == null || currentFeature.isDone();
+    }
+}
